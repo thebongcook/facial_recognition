da21af5cb14d229aefd85b2edc515b8c736c0291
Jeff Geerling <geerlingguy@mac.com>
2021-09-13 13:13:55 -0500

    Adjustment.

--- facial_recognition.py
+++ facial_recognition.py
@@ -27,7 +27,7 @@ fps = FPS().start()
 
 # Start the video stream.
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('output.mjpg', fourcc, 10.0, (640,  480))
+out = cv2.VideoWriter('video.avi', fourcc, 10.0, (640,  480))
 
 # Loop over frames from the video file stream.
 while True:
