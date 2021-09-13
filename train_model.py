c1fcf5a7ce4e32a4ddfafc3e3aebc622dfb7c6d6
Jeff Geerling <geerlingguy@mac.com>
2021-09-13 12:35:05 -0500

    Work on linting the Python code.

--- train_model.py
+++ train_model.py
@@ -18,30 +18,30 @@ knownNames = []
 
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
-	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
-	name = imagePath.split(os.path.sep)[-2]
+    # extract the person name from the image path
+    print("[INFO] processing image {}/{}".format(i + 1,
+        len(imagePaths)))
+    name = imagePath.split(os.path.sep)[-2]
 
-	# load the input image and convert it from RGB (OpenCV ordering)
-	# to dlib ordering (RGB)
-	image = cv2.imread(imagePath)
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # load the input image and convert it from RGB (OpenCV ordering)
+    # to dlib ordering (RGB)
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model="hog")
+    # detect the (x, y)-coordinates of the bounding boxes
+    # corresponding to each face in the input image
+    boxes = face_recognition.face_locations(rgb,
+        model="hog")
 
-	# compute the facial embedding for the face
-	encodings = face_recognition.face_encodings(rgb, boxes)
+    # compute the facial embedding for the face
+    encodings = face_recognition.face_encodings(rgb, boxes)
 
-	# loop over the encodings
-	for encoding in encodings:
-		# add each encoding + name to our set of known names and
-		# encodings
-		knownEncodings.append(encoding)
-		knownNames.append(name)
+    # loop over the encodings
+    for encoding in encodings:
+        # add each encoding + name to our set of known names and
+        # encodings
+        knownEncodings.append(encoding)
+        knownNames.append(name)
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
